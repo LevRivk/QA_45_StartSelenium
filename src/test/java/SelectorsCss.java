62460f878e7a5e6d69ff99c4ce3a943db4c5b96f
leva <shendonlevka@gmail.com>
2024-11-21 12:28:39 +0200

    HomeWorkHW4

---
+++ src/test/java/SelectorsCss.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SelectorsCss {
+
+    WebDriver driver = new ChromeDriver();
+    @Test
+    public void elementsHeaderIlCarro(){
+        driver.navigate().to("https://ilcarro.web.app/search");
+        WebElement elementTextHeader = driver.findElement(By.cssSelector("div[class='header']"));
+        System.out.println("text: "+ elementTextHeader.getText());
+
+
+        driver.quit();
+
+    }
+
+}
